91b057b312d055eab3f6ed26a3ff3de5cee676c5
kaique <kaiquesilva@codeby.com.br>
2021-03-02 21:37:46 -0300

    start moveit

---
+++ moveit/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/Button';
+
+
+function App() {
+  return (
+    <div >
+      <Button color="blue">Botão 1</Button>
+      <Button color="red">Botão 2</Button>
+      <Button color="yellow">Botão 3</Button>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ moveit/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+interface ButtonProps  {
+    color:string;
+    children:string;
+}
+
+export function Button( props:ButtonProps){
+    const counter = 1;
+    return(
+        <button type="button" 
+            style={{
+                backgroundColor:props.color
+            }}>
+            {props.children}
+            <strong>1</strong>
+        </button>
+    );
+}
